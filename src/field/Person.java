6bb65bc9c7611b4d93abb19c3ac087764fdb761b
SATISH <satish.gupta_cs18@gla.ac.in>
2020-02-24 21:15:48 -0800

    created constructor

--- src/field/Person.java
+++ src/field/Person.java
@@ -1,10 +1,17 @@
 package field;
 
+import node.MyLinkedList;
+
 public class Person {
     private String firstName;
     private String lastName;
     private String emailAddress;
     private MyLinkedList<String> contactNumber;
 
-
+    public Person(String firstName, String lastName, String emailAddress, MyLinkedList<String> contactNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailAddress = emailAddress;
+        this.contactNumber = contactNumber;
+    }
 }

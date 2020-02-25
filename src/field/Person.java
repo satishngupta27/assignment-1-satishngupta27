92d83017388cab17bcc12306460397f34d9f6a1a
SATISH <satish.gupta_cs18@gla.ac.in>
2020-02-24 21:12:19 -0800

    created fields of person class

---
+++ src/field/Person.java
@@ -0,0 +1,10 @@
+package field;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+    private MyLinkedList<String> contactNumber;
+
+
+}

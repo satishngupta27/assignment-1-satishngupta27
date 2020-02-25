4c7b260f2c8f1c629a24ae7667fd367f29d5a3a3
SATISH <satish.gupta_cs18@gla.ac.in>
2020-02-24 21:35:45 -0800

    created insert method in MyLinkedList

--- src/node/MyLinkedList.java
+++ src/node/MyLinkedList.java
@@ -33,4 +33,18 @@ public class MyLinkedList<E> {
     }
 
 
+    public void insert(E data, int index) {
+        add(data, index);
+    }
+
+    @Override
+    public void insert(E data) {
+        if (head == null) {
+            addFirst(data);
+        } else {
+            add(data, size);
+        }
+    }
+
+
 }
